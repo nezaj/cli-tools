619c18ce92f0631caeb5cf2264c1f8dffb478910
Joe Averbukh <joeaverbukh@gmail.com>
2014-08-06 22:29:06 -0700

    Refactor daily log generators

--- gen_log.py
+++ gen_log.py
@@ -2,61 +2,67 @@
 """
 CLI tool for quickly generating my daily log templates.
 
-TODO: Write tests so you can refactor
-TODO: Use a class-based approach for generating different logs
+TODO: Write tests!
 """
+import subprocess
 from datetime import datetime, timedelta
 
 # Let's try using click instead of argparse
 import click
 
-# Used for generating and outputting dates
-INPUT_DATE_FORMAT = '%m/%d/%y'
-OUTPUT_DATE_FORMAT = '%m/%d/%y (%A)'
+class BaseLog(object):
+    """
+    Base object for Log generator objects.
 
-# These are the daily logs I maintain
-DAILY_LOGS = ['personal', 'programming', 'sixpack']
+    This class defines methods and helpers to generate
+    daily logging entries using properties specified
+    by subclasses which inherit from it.
+    """
 
-# The base template for generating my daily logs
-DAILY_LOG_TEMPLATE = 'logs/daily_base.tmpl'
+    # The base template for generating my daily logs
+    DAILY_LOG_TEMPLATE = 'logs/daily_base.tmpl'
 
-# Output files for generated templates
-PERSONAL_LOG_OUTPUT = 'logs/personal_log.html'
-PROGRAMMING_LOG_OUTPUT = 'logs/programming_log.html'
-SIXPACK_LOG_OUTPUT = 'logs/sixpack_log.html'
+    @classmethod
+    def generate_entries(cls, dates):
+        with open(cls.LOG_OUTPUT, 'w') as f:
+            content = cls._render_daily_entries(dates, cls.TAGS)
+            f.write(content)
 
-def _get_template(tmpl):
-    """ Returns an unrendered jinja2 template object"""
-    template_env = get_jinja_env()
-    return template_env.get_template(tmpl)
+    @staticmethod
+    def _get_template(tmpl):
+        """ Returns an unrendered jinja2 template object """
+        template_env = get_jinja_env()
+        return template_env.get_template(tmpl)
+
+    @classmethod
+    def _render_daily_entries(cls, dates, tags):
+        template = cls._get_template(cls.DAILY_LOG_TEMPLATE)
+        return template.render(dates=dates, tags=tags)
+
+class PersonalLog(BaseLog):
+
+    TAGS = ['Social', 'Felt', 'Done']
+    LOG_OUTPUT = 'logs/personal_log.html'
+
+class ProgrammingLog(BaseLog):
+
+    TAGS = ['Productive', 'Done']
+    LOG_OUTPUT = 'logs/programming_log.html'
+
+class SixPackLog(BaseLog):
+
+    TAGS = ['Exercise', 'Sugar', 'Diet']
+    LOG_OUTPUT = 'logs/sixpack_log.html'
 
 def date_to_string(do):
-    """ Returns formatted string based on OUTPUT_DATE_FORMAT from datetime object """
+    """ Returns formatted string representation of a date from a datetime object """
     return do.strftime(OUTPUT_DATE_FORMAT)
-
-def generate_personal_entries(dates):
-    tags = ['Social', 'Felt', 'Done']
-    with open(PERSONAL_LOG_OUTPUT, 'w') as f:
-        content = render_daily_entries(dates, tags)
-        f.write(content)
-
-def generate_programming_entries(dates):
-    tags = ['Productive', 'Done']
-    with open(PROGRAMMING_LOG_OUTPUT, 'w') as f:
-        content = render_daily_entries(dates, tags)
-        f.write(content)
-
-def generate_sixpack_entries(dates):
-    tags = ['Exercise', 'Sugar', 'Diet']
-    with open(SIXPACK_LOG_OUTPUT, 'w') as f:
-        content = render_daily_entries(dates, tags)
-        f.write(content)
 
 def get_jinja_env(path=None):
     """
     Returns jinja2 environment object that loads templates from the specified path.
 
-    If no path provided then uses the invoked modules directory by default
+    If no path provided then uses the directory of the module that invoked this method
     """
     import os
     import jinja2
@@ -67,33 +73,39 @@ def get_jinja_env(path=None):
     template_loader = jinja2.FileSystemLoader(path)
     return jinja2.Environment(loader=template_loader)
 
-def render_daily_entries(dates, tags):
-    template = _get_template(DAILY_LOG_TEMPLATE)
-    return template.render(dates=dates, tags=tags)
-
 def to_datetime(s):
-    """ Returns datetime object from string with INPUT_DATE_FORMAT """
+    """ Returns datetime object from a formatted string """
     return datetime.strptime(s, INPUT_DATE_FORMAT)
 
 def today():
+    """ Returns date object representing the current date """
     return datetime.now().date()
 
 # I like using -h for help as well
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+# These are the daily logs I maintain
+DAILY_LOGS = ['personal', 'programming', 'sixpack']
+
+# Used for generating and outputting dates
+INPUT_DATE_FORMAT = '%m/%d/%y'
+OUTPUT_DATE_FORMAT = '%m/%d/%y (%A)'
+
+# Use this dictionary to fetch the desired log generator -- much cleaner than using a bunch of if/else statements
+LOG_DICT = {
+    'personal': PersonalLog,
+    'programming': ProgrammingLog,
+    'sixpack': SixPackLog
+}
+
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('log', type=click.Choice(DAILY_LOGS))
 @click.option('--date', default=today, help="Enter date as {} ex: 08/03/14".format(INPUT_DATE_FORMAT))
-@click.option('--days', default=7, type=int, help="Enter number of days to generate")
+@click.option('--days', default=7, type=int, help="Enter number of days to generate (default 7)")
 def main(log, date, days):
-    """
-    Generates a template for one of my daily log files.
+    """ Generates a template for one of my daily log files.  """
 
-    By default I expect to generate a week worth of entries. The generated
-    templates are saved in the logs folder
-    """
-
-    # If a date is provided it will be a string -- want to convert it to a datetime object
+    # If a date is specified on the command-line it will be a string -- want to convert it to a datetime object
     if date != today():
         date = to_datetime(date)
 
@@ -103,15 +115,11 @@ def main(log, date, days):
     dates = [date + timedelta(days=x) for x in range(days)]
     date_strings = reversed([date_to_string(d) for d in dates])
 
-    generate_log = generator_dict[log]
-    generate_log(date_strings)
+    log = LOG_DICT[log]
+    log.generate_entries(date_strings)
 
-# Use this dictionary to fetch the desired generator -- much cleaner than using a bunch of if/else statements
-generator_dict = {
-    'personal': generate_personal_entries,
-    'programming': generate_programming_entries,
-    'sixpack': generate_sixpack_entries
-}
+    # It's convenient to open the file once it's generated so I can copy and paste
+    subprocess.call(['open', log.LOG_OUTPUT])
 
 if __name__ == '__main__':
     main()
